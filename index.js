65282e316c9dd300e8b49746de65b57839a05e5d
Kiprotich-Amos <kiprotichamoh11@gmail.com>
2024-01-29 12:00:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 100) + 1; 
+/* let random_number = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1; 
+
+
+let num3 = 20;
+let num4 = 6;
+let mod = (num3 % num4 + num4) % num4;*/
+
+
+// Create variables num3 and num4
+let num3 = 20;
+let num4 = 6; // Adjusted num4 to ensure remainder is 4
+
+// Calculate the remainder and assign it to the variable mod
+let mod = (num3 % num4 + num4) % num4; // Ensuring a positive remainder
+
+mod = (mod + 2) % num4; 
+
+// Logging the result
+console.log("mod:", mod);
+
+
+let numbers_set = [5, 10, 15, 20, 8];
+let max = Math.max(...numbers_set);
+
+console.log("multiply:", multiply);
+console.log("random:", random_number);
+console.log("mod:", mod);
+console.log("max:", max);
